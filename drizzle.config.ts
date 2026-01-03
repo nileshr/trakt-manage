01c4b54f0b5eac1160784021b84df2081567bd30
Nilesh Rathi <nileshrathi01@gmail.com>
2026-01-03 16:14:39 +0530

    feat: make Drizzle DB file configurable with a default fallback.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
+const DB_FILE_NAME = process.env.DB_FILE_NAME || "trakt.db";
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: DB_FILE_NAME,
   },
 });
